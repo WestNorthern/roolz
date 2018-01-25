64ccaffb26d2ff988af5c8d35a44220baefcf2c1
westnorthern <rrichardson610@gmail.com>
2018-01-25 13:37:00 -0700

    spec for Rool::Basic from_json

--- spec/lib/rool/basic_spec.rb
+++ spec/lib/rool/basic_spec.rb
@@ -21,4 +21,24 @@ describe "Rool::Blank" do
     end
   end
 
+  describe '#from_json' do
+
+    it 'converts to a Basic class from json with ^o key of basic' do
+      @basic_test = Rool::Basic.new.to_json
+      expect(Rool::Basic.from_json(@basic_test)).to be_a_kind_of(Rool::Basic)
+    end
+    it 'converts to a Basic class from json with ^o key of a nested' do
+      @nested_test = Rool::Email.new.to_json
+      expect(Rool::Basic.from_json(@nested_test)).to be_a_kind_of(Rool::Basic)
+    end
+    it 'converts to a nested class from json with ^o key of the same nested class' do
+      @nested_test = Rool::Email.new.to_json
+      expect(Rool::Email.from_json(@nested_test)).to be_a_kind_of(Rool::Email)
+    end
+    it 'returns nil when trying to convert a nested class from json with ^o key of a different nested class' do
+      @nested_test = Rool::Email.new.to_json
+      expect(Rool::Equal.from_json(@nested_test)).to be(nil)
+    end
+  end
+
 end
